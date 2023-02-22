f31cfc1be686118ed75eeae98ac845cb2153d0b5
yteng <yteng@123.com>
2023-02-22 20:10:02 +0800

    修改header区域

--- src/components/layouts/YinHeader.vue
+++ src/components/layouts/YinHeader.vue
@@ -87,7 +87,7 @@ export default defineComponent({
   align-items: center;
   font-size: 20px;
   color: #2c3e50;
-  background: #ffff;
+  background: #EC4141;
   box-shadow: 0px 0px 8px 2px rgba(0, 0, 0, 0.3);
 }
 
